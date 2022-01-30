785010f2d36106c6a434a83e0d611fc805996f1f
DJ Meph <norm.witte@gmail.com>
2022-01-30 09:06:46 +0000

    use typescript to generate build command

---
+++ backend/build.ts
@@ -0,0 +1,13 @@
+import { exec } from 'child_process';
+import { config } from '../infrastructure/lib/config';
+
+const command = [
+  'NODE_ENV=production',
+  `STRAPI_URL=https://${config.subdomain}-api.${config.domainName}`,
+  `STRAPI_ADMIN_URL=https://${config.subdomain}.${config.domainName}`,
+  'yarn build'
+].join(' ');
+
+const proc = exec(command);
+proc.stdout.on('data', (data) => console.log(data));
+proc.on('exit', (code) => process.exit(code));
